8e731c1a84a456de192220cc097f2e52ab58ca8b
jonijo06 <jonasjuergens6@gmail.com>
2022-01-18 14:38:20 +0100

    complete 2015 Day1 Part2

---
+++ 2015/01/part-two.js
@@ -0,0 +1,21 @@
+'use strict';
+import fs from 'fs-extra';
+
+const rawInput = fs.readFileSync('./input.txt').toString();
+
+const input = rawInput.split('');
+
+let floor = 0;
+let result = 0;
+for (let i = 0; i < input.length; i++) {
+  if(input[i] === '(')
+    floor++;
+  else
+    floor--;
+  if(floor === -1){
+    
+    result = i +1;
+    break;
+  }
+}
+console.log("Result: " + result);
