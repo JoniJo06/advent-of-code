eef476583f82e995540354f39361f846f5d8fc5c
jonijo06 <jonasjuergens6@gmail.com>
2022-02-05 11:07:12 +0100

    complete 2020 Day1 Part2

---
+++ 2020/01/part-two.ts
@@ -0,0 +1,23 @@
+import fs from 'fs-extra';
+
+const rawInput = fs.readFileSync('./input.txt')
+  .toString();
+const input = rawInput.split(`
+`)
+  .map(el => Number(el));
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = i + 1; j < input.length; j++) {
+    for (let k = j + 1; k < input.length; k++) {
+      const num1 = input[i];
+      const num2 = input[j];
+      const num3 = input[k]
+      if (num1 + num2 + num3 === 2020) {
+        console.log(num1 * num2 * num3);
+        break;
+      }
+
+    }
+
+  }
+}
