0faf482821f06c872927b0d2dc0b307baeb656d0
jonijo06 <jonasjuergens6@gmail.com>
2022-02-11 15:36:14 +0100

    complete 2015 Day1 Part2 in haskell

---
+++ 2015/01/haskell/part-two.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Data.Typeable
+-- Convert String into an array of character
+charStrs :: String -> [String]
+charStrs = fmap pure
+
+for:: Int -> Int -> Int -> [String] -> Int -> IO ()
+for a b c input count = do
+  if a < b
+    then do
+      if count <= -1
+        then do
+          print a
+        else do
+      if ([['(']] == (take 1 (drop a input)))
+        then  for (a + c) b c input (count + 1)
+        else  for (a + c) b c input (count - 1)
+    else print count
+
+
+main :: IO ()
+main = do
+  let count = 0
+  rawInput <- readFile "../input.txt"
+
+  let input = charStrs rawInput
+
+  let count = 0
+  for 0 (length input) 1 input count
